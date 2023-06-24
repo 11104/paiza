439dfe4cdf7eb2d2cba1e0579f2a48dfd0634cc1
11104 <85434372+11104@users.noreply.github.com>
2023-06-24 22:33:08 +0900

    add 11js

---
+++ js/11-1.js
@@ -0,0 +1,8 @@
+// 文字列に一致しているか判定する
+let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+let greeting = lines[0];
+if(greeting=="Hello"){
+    console.log("こんにちは");
+}else{
+    console.log(`${greeting}はHelloではない`);
+}
---
+++ js/11-2.js
@@ -0,0 +1,10 @@
+// 文字列に一致しているか判定する
+let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+let greeting = lines[0];
+if(greeting=="Hello"){
+    console.log("こんにちは");
+}else if (greeting=="Good morning"){
+    console.log("おはよう");
+}else{
+    console.log("はじめまして");
+}

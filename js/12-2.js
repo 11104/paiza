a33dfeba47d79dcad715366356e9abb6f0e21d84
11104 <85434372+11104@users.noreply.github.com>
2023-06-24 22:43:08 +0900

    add 12js

---
+++ js/12-2.js
@@ -0,0 +1,10 @@
+// 数値の大きさを判定する
+let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+let number = lines[0];
+if(number==100){
+    console.log(number);
+}else if(number>100){
+    console.log("100より大きい");
+}else{
+    console.log("100以下です");
+}

812b3e024b02c51950b4325407df849e306af9cb
11104 <85434372+11104@users.noreply.github.com>
2023-06-25 15:15:31 +0900

    add jspr1 2

---
+++ js/primer1/2-2.js
@@ -0,0 +1,7 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// ここに、出力するコードを書く
+/*
+console.log(”hello paizaラーニング");
+*/
+console.log("hello paizaラーニング");
---
+++ js/primer1/2-3.js
@@ -0,0 +1,8 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// ここに、出力するコードを書く
+/*
+Console.log("hello paizaラーニング");
+*/
+console.log("hello paizaラーニング");
+

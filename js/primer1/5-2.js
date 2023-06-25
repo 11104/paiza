6a242370d623329c3b932e3eea294301845bf950
11104 <85434372+11104@users.noreply.github.com>
2023-06-25 15:30:37 +0900

    add jspr1 5

---
+++ js/primer1/5-2.js
@@ -0,0 +1,8 @@
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var player = "勇者";
+/*
+console.log(player0 + "の体力が回復した");
+*/
+console.log(player + "の体力が回復した");
---
+++ js/primer1/5-3.js
@@ -0,0 +1,9 @@
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var team = "勇者と戦士";
+/*
+console.log(player + "の体力が回復した");
+*/
+console.log(team + "の体力が回復した");
+

74121d12dcf4050cb7c2c04efc4ab8a32ddd287b
11104 <85434372+11104@users.noreply.github.com>
2023-07-06 18:18:47 +0900

    add jspr3 dir

---
+++ js/primer3/1-1.js
@@ -0,0 +1,9 @@
+// 「ハローpaizaラーニング」と10回表示する
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var count=0;
+while(count<10){
+    console.log("ハローpaizaラーニング");
+    count++;
+}
---
+++ js/primer3/1-2.js
@@ -0,0 +1,9 @@
+// 数値を0から15まで表示する
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var count=0;
+while(count<=15){
+    console.log(count);
+    count++;
+}
